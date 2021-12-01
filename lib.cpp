bb983c20369b8e18b0f048ce7316a2eb3642bd7e
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-01 13:07:40 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
-
+#include "lib.h"
+bool numeri_primi(int  n=0){
+    int div=1, conta=0;
+    while(conta<=1 && div<=n/2) {
+        if(n%div==0)
+        {
+            conta++;
+        }
+        div++;
+    }
+    if (conta==1){
+        return true;
+    } else {
+        return false;
+    }
+}
