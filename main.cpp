d531ee007bc0da1771c676df03db248748f2bac8
Stefano Spinetta <91751558+Diferus@users.noreply.github.com>
2021-12-01 13:06:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
+int main() {
+    int  n=0;
 
-int main(){
-  return 0;
+    do {
+        cin >> n;
+    }while (n<1);
+    
+    if (numeri_primi(n)){
+        cout << "numero primo" << endl;
+    }else{
+        cout << "numero non primo" << endl;
+    }
 }
